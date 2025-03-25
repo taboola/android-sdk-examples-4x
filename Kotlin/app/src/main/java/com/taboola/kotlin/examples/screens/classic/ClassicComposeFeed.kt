cf6a95495172773e93f9c3766e6b7439dd727c1c
Tomer Ben-Rachel <tomer.b@taboola.com>
2025-03-25 16:29:26 +0200

    feature/MOB-4366-Update-Project-Compose: Refactoring ClassicComposeFeed screen to include a compose unit

--- Kotlin/app/src/main/java/com/taboola/kotlin/examples/screens/classic/ClassicComposeFeed.kt
+++ Kotlin/app/src/main/java/com/taboola/kotlin/examples/screens/classic/ClassicComposeFeed.kt
@@ -5,14 +5,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.stringResource
 import androidx.fragment.app.Fragment
+import com.taboola.android.TBLClassicComposeUnit
 import com.taboola.android.TBLClassicPage
-import com.taboola.android.TBLClassicUnit
 import com.taboola.android.Taboola
 import com.taboola.android.annotations.TBL_PLACEMENT_TYPE
 import com.taboola.android.listeners.TBLClassicListener
@@ -28,9 +31,12 @@ class ClassicComposeFeed : Fragment() {
     ): View? = ComposeView(requireContext()).apply {
         setContent {
             // Create and return a Taboola Unit
-            val tblClassicUnit = getTaboolaUnit(PlacementInfo.classicFeedProperties())
-            ListScopeSample(tblClassicUnit = tblClassicUnit)
-            tblClassicUnit.fetchContent()
+            val tblClassicComposeUnit = getTaboolaUnit(PlacementInfo.classicFeedProperties())
+            // Fetch content for Unit
+            tblClassicComposeUnit.fetchContent()
+            //Add TBLClassicUnit to the UI (to layout)
+            ListScopeSample(tblClassicComposeUnit = tblClassicComposeUnit)
+
         }
     }
 
@@ -38,13 +44,13 @@ class ClassicComposeFeed : Fragment() {
      * Define a Page that represents this screen, get a Unit from it, add it to screen and fetch its content
      * Notice: A Unit of limited items, called "Widget" in Taboola, can be set in TBL_PLACEMENT_TYPE.PAGE_BOTTOM or TBL_PLACEMENT_TYPE.PAGE_MIDDLE
      */
-    private fun getTaboolaUnit(properties: PlacementInfo.ClassicFeedProperties): TBLClassicUnit {
+    private fun getTaboolaUnit(properties: PlacementInfo.ClassicFeedProperties): TBLClassicComposeUnit {
         // Define a page to control all Unit placements on this screen
         val classicPage: TBLClassicPage =
             Taboola.getClassicPage(properties.pageUrl, properties.pageType)
 
         // Define a single Unit to display
-        val classicUnit: TBLClassicUnit = classicPage.build(
+        val classicUnit: TBLClassicComposeUnit = classicPage.buildComposeUnit(
             context,
             properties.placementName,
             properties.mode,
@@ -59,7 +65,8 @@ class ClassicComposeFeed : Fragment() {
                     super.onAdReceiveFail(error)
                     Log.d(TAG, "Taboola | onAdReceiveFail: $error")
                 }
-            })
+            }
+        )
         return classicUnit
     }
 
@@ -70,17 +77,19 @@ class ClassicComposeFeed : Fragment() {
 
 
 @Composable
-fun ListScopeSample(tblClassicUnit: TBLClassicUnit) {
-    LazyColumn {
+fun ListScopeSample(tblClassicComposeUnit: TBLClassicComposeUnit) {
 
+    val lazyListState = rememberLazyListState()
+
+    LazyColumn(modifier = Modifier.fillMaxSize(), lazyListState) {
         // Add item
         item() {
             Text(text = stringResource(id = R.string.lorem_ipsum_long))
         }
 
         // Add another single item (Taboola Feed)
-        item() {
-            classicIntegration(tblClassicUnit = tblClassicUnit)
+        item {
+            tblClassicComposeUnit.GetClassicUnitView(state = lazyListState)
         }
     }
 }
